5999056d4c83ea6532b2057a0a2e5afb2375421f
DennoFreaky <dennischeptumo@gmail.com>
2022-10-11 14:49:16 +0300

    init

--- vehicle/v_details.php
+++
@@ -1,7 +0,0 @@
-                                    <div class="col fw-bold m-1 ">
-                                        <div class="row">
-                                            <div class="col"><a href="">Details</a></div>
-                                            <div class="col"><a href="">Engine & Transmission</a></div>
-                                            <div class="col"><a href="">Engine & Transmission</a></div>
-                                        </div>
-                                    </div>
